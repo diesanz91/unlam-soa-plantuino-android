e4af5f0d47f94f8c2cbc5a934424dc49f89f91cb
diesanz91 <diego_sanz@live.com.ar>
2017-05-25 18:20:48 -0300

    Agregado de funcionalidad de conexion Bluetooth

--- Plantuino/app/src/main/java/com/plantuino/unlam_soa/plantuino/MainActivity.java
+++ Plantuino/app/src/main/java/com/plantuino/unlam_soa/plantuino/MainActivity.java
@@ -1,6 +1,8 @@
 package com.plantuino.unlam_soa.plantuino;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,12 +13,24 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.speech.RecognizerIntent;
 import android.content.ActivityNotFoundException;
+
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.UUID;
+
+import android.bluetooth.*;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int RECOGNIZE_SPEECH_ACTIVITY = 1;
     private String voz;
+    private String direccion = null;
+    private ProgressDialog progreso;
+    private BluetoothAdapter bluetoothAdapter = null;
+    private BluetoothSocket bluetoothSocket = null;
+    private boolean isBtConnected = false;
+    private static final UUID mUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    private Button btnDesconectarBluetooth = null;
     private Button btnVentilacion = null;
     private String txtBtnVentilacion = null;
     private Button btnIluminacion = null;
@@ -34,9 +48,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+
+        //Obtenemos direccion MAC del dispositivo Bluetooth seleccionado de la lista
+        direccion = intent.getStringExtra(ManejadorBluetoothActivity.EXTRA_ADDRESS);
+
         setContentView(R.layout.activity_main);
 
         //Obtengo los diversos componentes de la vista
+        btnDesconectarBluetooth = (Button)findViewById(R.id.btnDesconectarBluetooth);
         btnVentilacion = (Button)findViewById(R.id.btnVentilacion);
         txtBtnVentilacion = (String)btnVentilacion.getText();
         btnIluminacion = (Button)findViewById(R.id.btnIluminacion);
@@ -59,6 +80,60 @@ public class MainActivity extends AppCompatActivity {
         btnMensajeVoz.setOnClickListener(botonesListener);
         btnLimpiarDatos.setOnClickListener(botonesListener);
 
+        //Ejecutamos conexion Bluetooth
+        new conexionBluetooth().execute();
+
+    }
+
+    private class conexionBluetooth extends AsyncTask<Void, Void, Void>{
+
+        private boolean conexionExitosa = true;
+
+        protected void onPreExecute(){
+            progreso = ProgressDialog.show(MainActivity.this,"Conectando...", "Espere por favor!");
+        }
+
+        protected Void doInBackground(Void... dispositivos){
+            try{
+                if(bluetoothSocket == null || !isBtConnected){
+                    bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+                    //Realizamos conexion con el dispositivo y verificamos si esta disponible
+                    BluetoothDevice dispositivo = bluetoothAdapter.getRemoteDevice(direccion);
+                    bluetoothSocket = dispositivo.createInsecureRfcommSocketToServiceRecord(mUUID);
+                    BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
+                    bluetoothSocket.connect();
+                }
+
+            }catch (IOException ioExc){
+                conexionExitosa = false;
+            }
+            return null;
+        }
+
+        protected void onPostExecute(Void result){
+            super.onPostExecute(result);
+
+            if(!conexionExitosa){
+                Toast.makeText(getApplicationContext(),"Falló la conexión",Toast.LENGTH_SHORT).show();
+                finish();
+            } else {
+               Toast.makeText(getApplicationContext(),"Conexión exitosa",Toast.LENGTH_SHORT).show();
+                isBtConnected = true;
+            }
+            progreso.dismiss();
+        }
+
+    }
+
+    private void desconexionBluetooth(){
+        if(bluetoothSocket != null){
+            try{
+                bluetoothSocket.close();
+            }catch (IOException ioExc){
+                Toast.makeText(getApplicationContext(),"Ocurrió un error al desconectar",Toast.LENGTH_SHORT).show();
+            }
+        }
+        finish();
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
@@ -102,6 +177,9 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View v){
 
             switch (v.getId()){
+                case  R.id.btnDesconectarBluetooth:
+                    desconexionBluetooth();
+                    break;
                 case R.id.btnVentilacion:
                     if(txtBtnVentilacion.compareToIgnoreCase("ENCENDER") == 0){
                         txtBtnVentilacion = "APAGAR";
---
+++ Plantuino/app/src/main/java/com/plantuino/unlam_soa/plantuino/ManejadorBluetoothActivity.java
@@ -0,0 +1,94 @@
+package com.plantuino.unlam_soa.plantuino;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.bluetooth.*;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.Set;
+
+public class ManejadorBluetoothActivity extends AppCompatActivity{
+
+    private BluetoothAdapter bluetoothAdapter = null;
+    private Button btnDispositivosBluetooth = null;
+    private ListView listaDispositivosBluetooth = null;
+    public  static String EXTRA_ADDRESS = "direccionMAC_dispositivo";
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        btnDispositivosBluetooth = (Button)findViewById(R.id.btnDispositivosBluetooth);
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (bluetoothAdapter == null){
+            //Significa que el dispositivo no soporta Bluetooth
+            Toast.makeText(getApplicationContext(), "Bluetooth no soportado", Toast.LENGTH_SHORT).show();
+        } else {
+            //Si dispositivo soporta Bluetooth, verificamos si se encuentra habilitado
+            if(!bluetoothAdapter.isEnabled()){
+                //Si no se encuentra habilitado solicitamos al usuario si desea hacerlo
+                Intent habilitarBluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(habilitarBluetoothIntent,1);
+            }
+        }
+
+        btnDispositivosBluetooth.setOnClickListener(botonesListener);
+    }
+
+    private View.OnClickListener botonesListener= new View.OnClickListener(){
+
+        public void onClick(View v){
+            switch (v.getId()){
+                case R.id.btnDispositivosBluetooth:
+                    listarDispositivosBluetooth();
+                    break;
+                default:
+                    Toast.makeText(getApplicationContext(),"Error ocurrido en Listener de botones",Toast.LENGTH_LONG).show();
+            }
+        }
+
+    };
+
+    private void listarDispositivosBluetooth(){
+        //Verificamos si existe algun dispositivo sincronizado
+        Set<BluetoothDevice> dispositivosBluetoothSincronizados = bluetoothAdapter.getBondedDevices();
+        ArrayList listaDispositivosBluetoothSincronizados = new ArrayList();
+        if(dispositivosBluetoothSincronizados.size() > 0){
+            for(BluetoothDevice dispositivo : dispositivosBluetoothSincronizados){
+                //A cada dispositivo sincronizado lo agrego a mi lista de dispositivos Bluetooth mediante nombre y direccion MAC del dispositivo
+                listaDispositivosBluetoothSincronizados.add(dispositivo.getName() + "\n" + dispositivo.getAddress());
+            }
+        } else {
+            Toast.makeText(getApplicationContext(),"No se encontraron dispositivos Bluetooth sincronizados",Toast.LENGTH_LONG).show();
+        }
+
+        //Armo lista de dispositivos Bluetooth sincronizados
+        final ArrayAdapter dispositivosBluetoothAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, listaDispositivosBluetoothSincronizados);
+        listaDispositivosBluetooth.setAdapter(dispositivosBluetoothAdapter);
+        listaDispositivosBluetooth.setOnItemClickListener(listViewListener);
+    }
+
+    private AdapterView.OnItemClickListener listViewListener = new AdapterView.OnItemClickListener(){
+        public void onItemClick (AdapterView<?> av, View v, int arg2, long arg3){
+            //Obtengo la direccion MAC del item seleccionado, los ultimos 17 caracteres del item corresponden a la direccion MAC
+            String info = ((TextView) v).getText().toString();
+            String direccion = info.substring(info.length()-17);
+
+            //Creamos un intent para iniciar la siguiente Activity
+            Intent intent = new Intent(ManejadorBluetoothActivity.this, MainActivity.class);
+
+            //Cambiamos de Activity
+            intent.putExtra(EXTRA_ADDRESS, direccion);
+            startActivity(intent);
+        }
+    };
+
+}
